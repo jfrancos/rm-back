ddbfd24f0bd0b776acb8c2c8f8a34562118e59fd
Justin Francos <justinfrancos@gmail.com>
2018-10-09 23:30:31 -0400

    updated message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 let port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send(`Example app listening on port ${port}!`);
 });
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
