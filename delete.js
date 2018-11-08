b9ff18843fcdb7e4c1ebe97c815cb2fdf579dc21
Justin Francos <justinfrancos@gmail.com>
2018-11-07 19:07:07 -0500

    update delete.js to use .env, and remove all users instead of just first 100

--- delete.js
+++ delete.js
@@ -1,12 +1,34 @@
 #!/usr/local/bin/node
 
-var stripe = require("stripe")("sk_test_4vUzfLsfzZ7ffojQgISR1ntd");
+const dotenv = require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 
-stripe.customers.list(
-  { limit: 100,
-  	email: 'justinfrancos@gmail.com' },
-  function(err, customers) {
-    customers.data.forEach((customer) => stripe.customers.del(customer.id).catch((err) => console.log('Customer deletion error:\n',
-			(({ rawType, code, param, message, detail }) => ({ rawType, code, param, message, detail })) (err))));
-  }
-);
+delete_customers = async () => {
+	const customers = await stripe.customers.list({ limit: 100, email: 'justinfrancos@gmail.com' });
+	const data = customers.data;
+	if (data.length > 0) {
+    	data.forEach( async (customer) => {
+    		try {
+    			await stripe.customers.del(customer.id);
+    		} catch(err) {
+				console.log('Customer deletion error:\n',
+				(({ rawType, code, param, message, detail }) => ({ rawType, code, param, message, detail })) (err))
+			}
+		})
+		console.log('Deleting more customers in 5 seconds');
+		setTimeout( delete_customers, 5000 );
+	}
+}
+
+delete_customers();
+
+
+
+// stripe.customers.list(
+//   { limit: 100,
+//   	email: 'justinfrancos@gmail.com' },
+//   function(err, customers) {
+//     customers.data.forEach((customer) => stripe.customers.del(customer.id).catch((err) => console.log('Customer deletion error:\n',
+// 			(({ rawType, code, param, message, detail }) => ({ rawType, code, param, message, detail })) (err))));
+//   }
+// );
