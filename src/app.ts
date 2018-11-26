c656f8af214b8c10c3ba3656c77fb1207ac1cc69
Justin Francos <justinfrancos@gmail.com>
2018-11-26 00:19:40 -0500

    setup subscription updates via stripe webhooks + testing

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
-import assert from 'assert';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
+import assert from "assert";
+import bodyParser from "body-parser";
+import "colors";
+import dotenv from "dotenv";
 import express, { Request, Response, NextFunction, Application } from "express";
-import helmet from 'helmet';
-import plainJoi from 'joi';
-const joiZxcvbn = require('joi-zxcvbn');
-import sodium from 'libsodium-wrappers-sumo';
-import Mailgun from 'mailgun-js';
-import mongodb from 'mongodb';
-import Stripe, { IStripeError, customers, subscriptions, ICard } from 'stripe';
-import zxcvbn from 'zxcvbn';
-import * as http from 'http'
+import helmet from "helmet";
+import plainJoi from "joi";
+const joiZxcvbn = require("joi-zxcvbn");
+import sodium from "libsodium-wrappers-sumo";
+import Mailgun from "mailgun-js";
+import mongodb from "mongodb";
+import Stripe, { IStripeError, customers, subscriptions, ICard } from "stripe";
+import zxcvbn from "zxcvbn";
+import * as http from "http";
+// import _  from 'lodash';
+var diff = require("deep-diff").diff;
 
 dotenv.config();
 type Customer = customers.ICustomer;
@@ -20,7 +23,7 @@ const token_window = 60000;
 const joi = plainJoi.extend(joiZxcvbn(plainJoi));
 
 // Mailgun Init
-const domain = 'mg.rhythmandala.com';
+const domain = "mg.rhythmandala.com";
 const apiKey = process.env.MG_KEY;
 const mailgun = new Mailgun({ apiKey, domain });
 const emailBody1 = `<p>Dear Rhythm Aficionado,
@@ -28,10 +31,9 @@ const emailBody1 = `<p>Dear Rhythm Aficionado,
 rhythmic structures from all across the globe simple, accessible and playable \
 so that everyone can enjoy the benefits of understanding and experiencing \
 rhythm in the body. If you have any questions, please email ryan@tapgym.com.
-	<p>Go ahead and <a href='https://app.rhythmandala.com?`
+	<p>Go ahead and <a href='https://app.rhythmandala.com?`;
 const emailBody2 = `'>click this link to complete your registration</a> and \
-have fun!`
-
+have fun!`;
 
 // Stripe Init
 const stripe = new Stripe(process.env.STRIPE_KEY);
@@ -42,16 +44,20 @@ let users: mongodb.Collection;
 let server: mongodb.MongoClient;
 const mongodb_uri = process.env.MONGODB_URI;
 const mongoInit = async () => {
-	server = await mongodb.connect(mongodb_uri, { useNewUrlParser: true });
-	users = await server.db().collection('users');
-	await users.createIndex( { 'email': 1 }, { unique: true } );
+	server = await mongodb.connect(
+		mongodb_uri,
+		{ useNewUrlParser: true }
+	);
+	users = await server.db().collection("users");
+	await users.createIndex({ email: 1 }, { unique: true });
+	await users.createIndex({ stripe_id: 1 }, { unique: true });
 };
 
 // Express Init
 const app = express();
 const port = process.env.PORT;
 let expressServer: http.Server;
-app.use(helmet())
+app.use(helmet());
 app.use(bodyParser.json());
 
 // Start DB then Express then Mocha callback
@@ -61,15 +67,19 @@ app.use(bodyParser.json());
 	if (mocha_callback) {
 		mocha_callback();
 	}
-})()
+})();
 
-app.use('/auth', async (req: Request, res: Response, next: NextFunction) => {
+app.use("/auth", async (req: Request, res: Response, next: NextFunction) => {
 	const { email } = req.body;
-	const user = await users.findOne({email: req.body.email});
+	const user = await users.findOne({ email: req.body.email });
 	const valid_until = req.body.valid_until;
-	const isValid = sodium.crypto_auth_verify(from_base64(req.body.mac), valid_until, user['signing_key'].buffer);
-	if (!isValid || (Date.now() > parseInt(valid_until))) {
-		res.send({error: 'invalid'});
+	const isValid = sodium.crypto_auth_verify(
+		from_base64(req.body.mac),
+		valid_until,
+		user["signing_key"].buffer
+	);
+	if (!isValid || Date.now() > parseInt(valid_until)) {
+		res.send({ error: "invalid" });
 	} else {
 		next();
 	}
@@ -81,51 +91,60 @@ app.use('/auth', async (req: Request, res: Response, next: NextFunction) => {
 // 	res.send();
 // });
 
-app.post('/auth/test', async (req: Request, res: Response) => {
+app.post("/auth/test", async (req: Request, res: Response) => {
 	const { email } = req.body;
 	const user = await users.findOne({ email });
-	res.send(generateTokenDict(user['signing_key'].buffer));
+	res.send(generateTokenDict(user["signing_key"].buffer));
 });
 
-app.post('/auth/refresh_auth_key', async (req: Request, res: Response) => {
+app.post("/auth/refresh_auth_key", async (req: Request, res: Response) => {
 	const { email } = req.body;
-	const user = await users.findOneAndUpdate( { email }, {
-		$set: {
-			signing_key: Buffer.from(sodium.crypto_auth_keygen())
+	const user = await users.findOneAndUpdate(
+		{ email },
+		{
+			$set: {
+				signing_key: Buffer.from(sodium.crypto_auth_keygen())
+			}
 		}
-	});
-	res.send({ message: 'invalid' });
+	);
+	res.send({ message: "invalid" });
 });
 
 const signupSchema = joi.object().keys({
-    password: joi.string().zxcvbn(3).required(),
-    source: joi.string().required(),
-    email: joi.string().email().required()
+	password: joi
+		.string()
+		.zxcvbn(3)
+		.required(),
+	source: joi.string().required(),
+	email: joi
+		.string()
+		.email()
+		.required()
 });
 
-app.post('/signup', async (req: Request, res: Response) => {
-	const validation = signupSchema.validate(req.body)
+app.post("/signup", async (req: Request, res: Response) => {
+	const validation = signupSchema.validate(req.body);
 	if (validation.error) {
-		handleError(req, res, validation.error.name, validation.error.message );
+		handleError(req, res, validation.error.name, validation.error.message);
 		return;
 	}
 
 	const { email, password, source } = validation.value;
 	let user = await users.findOne({ email });
 	if (user) {
-		handleError(req, res, 'DuplicateUserError', 'User already exists')
+		handleError(req, res, "DuplicateUserError", "User already exists");
 		return;
 	}
 
 	let customer: Customer;
 	try {
-		logMessage(req, 'Awaiting customer creation');
+		logMessage(req, "Awaiting customer creation");
 		customer = await stripe.customers.create({ source, email });
 	} catch (err) {
 		handleError(req, res, err.type, err.message);
-	 	return;
+		return;
 	}
-	const card = customer.sources.data.find (
+	const card = customer.sources.data.find(
 		source => source.id === customer.default_source
 	) as ICard;
 	logMessage(req, `Successful signup with username [${email}]`);
@@ -133,140 +152,212 @@ app.post('/signup', async (req: Request, res: Response) => {
 	const signing_key = Buffer.from(sodium.crypto_auth_keygen());
 	try {
 		const pwhash = sodium.crypto_pwhash_str(
-	 		req.body.password,
-		 	sodium.crypto_pwhash_OPSLIMIT_SENSITIVE,
-			sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE);
+			req.body.password,
+			sodium.crypto_pwhash_OPSLIMIT_SENSITIVE,
+			sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE
+		);
 		await users.insertOne({
-			email, signing_key, confirmation_key, pwhash,
+			email,
+			signing_key,
+			confirmation_key,
+			pwhash,
 			stripe_id: customer.id,
 			brand: card.brand,
 			last4: card.last4,
 			exp_year: card.exp_year,
-			exp_month: card.exp_month
+			exp_month: card.exp_month,
+			//	stripe_cust: customer,
+			current_period_end: 0,
+			shape_capacity: 0,
+			monthly_prints: 0,
+			extra_prints: 0,
+			shapes: {}
 		});
 	} catch (err) {
 		res.status(400).json(distillError(err));
 		return;
 	}
 	const data = {
-	  from: 'RhythMandala <signups@rhythmandala.com>',
-	  to: email,
-	  subject: 'Follow Link to Complete Signup',
-	  html: `${emailBody1}email=${email}&key=${confirmation_key}${emailBody2}`
+		from: "RhythMandala <signups@rhythmandala.com>",
+		to: email,
+		subject: "Follow Link to Complete Signup",
+		html: `${emailBody1}email=${email}&key=${confirmation_key}${emailBody2}`
 	};
 	const mg_response = await mailgun.messages().send(data);
 	console.log(mg_response);
 	res.send(generateTokenDict(signing_key));
 });
 
-
-				// await stripe.customers.del(customer.id);
-		// 	} catch (err) {
-		// 		console.error('SIGNUP: Customer deletion error:\n', distillError(err));
-		// 	}
-		// }
-		
-
-const confirmEmailSchema = joi.object().keys({ // how to make sure only these two are included
+const confirmEmailSchema = joi.object().keys({
+	// how to make sure only these two are included
 	email: joi.string().required(),
 	key: joi.string().required()
-})
+});
 
-app.post('/confirm_email', async function(req: Request, res: Response) {
+app.post("/confirm_email", async function(req: Request, res: Response) {
 	// Joi validation
-	const validation = confirmEmailSchema.validate(req.body)
+	const validation = confirmEmailSchema.validate(req.body);
 	if (validation.error) {
-		handleError(req, res, validation.error.name, validation.error.message );
+		handleError(req, res, validation.error.name, validation.error.message);
 		return;
 	}
 
-	const handleConfError = () => { // don't let on if an email exists
-		handleError(req, res, 'EmailConfirmationError', `There was an error confirming ${email}`)
-	}
+	const handleConfError = () => {
+		// don't let on if an email exists
+		handleError(
+			req,
+			res,
+			"EmailConfirmationError",
+			`There was an error confirming ${email}`
+		);
+	};
 
 	// Check if user exists
 	const { email, key } = validation.value;
-	const user = await users.findOne({email});
+	const user = await users.findOne({ email });
 	if (!user) {
 		handleConfError();
-		console.log( 'MissingUserError', `User ${email} does not exist`)
-	 	return;
+		console.log("MissingUserError", `User ${email} does not exist`);
+		return;
 	}
 
 	// Check if user is already confirmed
 	if (!user.confirmation_key) {
 		handleConfError();
-		console.log( 'UserAlreadyConfirmedError', 'User is already confirmed');
+		console.log("UserAlreadyConfirmedError", "User is already confirmed");
 		return;
 	}
 
 	// Check if key is correct
-	if (key != user['confirmation_key']) {
+	if (key != user["confirmation_key"]) {
 		handleConfError();
-		console.log('ConfirmationKeyError', `Key does not match`);
+		console.log("ConfirmationKeyError", `Key does not match`);
 		return;
 	}
 
 	// Update DB
-	await users.findOneAndUpdate({email: email}, { $unset: { confirmation_key: ''}});
+	await users.findOneAndUpdate(
+		{ email: email },
+		{ $unset: { confirmation_key: "" } }
+	);
 	console.log(`EMAIL_CONFIRMATION_SUCCESSFUL: with email [${email}]`);
 
 	// Create Stripe subscription
-	console.log('Awaiting subscription creation');
+	console.log("Awaiting subscription creation");
 	let subscription;
 	try {
 		subscription = await stripe.subscriptions.create({
 			customer: user.stripe_id,
-			items: [{ plan: 'plan_DrPVwslmSpiOT4' }]
+			items: [{ plan: "plan_DrPVwslmSpiOT4" }]
 		});
 	} catch (err) {
 		res.send();
 		handleError(req, null, err.type, err.message);
-	 	return;
+		return;
 	}
 
 	res.send();
 });
 
-app.post('/login', async function (req: Request, res: Response) {
-	const user = await users.findOne({email: req.body.email});
-	const isValid = sodium.crypto_pwhash_str_verify(user['pwhash'], req.body.password);
+const handleStripeWebhook = async (req: Request, res: Response) => {
+	//console.log(JSON.stringify(req.body, null, 4));
+	//console.log("New Webhook");
+	const object = req.body.data.object;
+	const customer_id = object.customer || object.id;
+	let stripe_user, user;
+	try {
+		stripe_user = await stripe.customers.retrieve(customer_id);
+		// console.log(JSON.stringify(stripe_user, null, 4));
+		user = await users.findOne({ stripe_id: customer_id });
+		if (!user) {
+			return;
+		}
+		//console.log(JSON.stringify(diff(user.stripe_cust, stripe_user), null, 4));
+		if (
+			stripe_user.subscriptions.total_count > 0 &&
+			stripe_user.subscriptions.data[0].current_period_end >
+				user.current_period_end
+		) {
+			const subscription = stripe_user.subscriptions.data[0];
+			const current_period_end = subscription.current_period_end;
+			const status = subscription.status;
+			const shape_capacity = user.shape_capacity + 5;
+			const monthly_prints = 5;
+			await users.findOneAndUpdate(
+				{ stripe_id: customer_id },
+				{
+					$set: {
+						shape_capacity,
+						monthly_prints,
+						current_period_end,
+						status
+					}
+				}
+			);
+		}
+		// await users.findOneAndUpdate(
+		// 	{ stripe_id: customer_id },
+		// 	{ $set: { stripe_cust: stripe_user } }
+		// );
+		user = await users.findOne({ stripe_id: customer_id });
+		//console.log(JSON.stringify(user, null, 4));
+		// console.log(customer_id.black.bgRed);
+		// console.log(req.body.type.black.bgRed);
+		res.send();
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+app.post("/stripe", handleStripeWebhook);
+
+app.post("/login", async function(req: Request, res: Response) {
+	const user = await users.findOne({ email: req.body.email });
+	const isValid = sodium.crypto_pwhash_str_verify(
+		user["pwhash"],
+		req.body.password
+	);
 	const email = req.body.email;
 	if (isValid) {
-		const dict = generateTokenDict(user['signing_key'].buffer);
+		const dict = generateTokenDict(user["signing_key"].buffer);
 		console.log(`LOGIN_SUCCESSFUL: with username [${email}]`);
-		res.send( dict );
+		res.send(dict);
 	} else {
 		console.log(`LOGIN_FAILED: with username [${email}]`);
-		res.send( { error: 'Login error' });
+		res.send({ error: "Login error" });
 	}
 });
 
 const generateTokenDict = (key: Buffer) => {
 	const valid_until = (Date.now() + token_window).toString();
 	const mac = sodium.crypto_auth(valid_until, key);
-	return ({ mac: to_base64(mac), valid_until: valid_until});
+	return { mac: to_base64(mac), valid_until: valid_until };
 };
 
 const from_base64 = (bytes: string) => {
-	return sodium.from_base64(bytes, sodium.base64_variants.URLSAFE_NO_PADDING )
-}
+	return sodium.from_base64(bytes, sodium.base64_variants.URLSAFE_NO_PADDING);
+};
 
 const to_base64 = (bytes: Uint8Array) => {
-	return sodium.to_base64(bytes, sodium.base64_variants.URLSAFE_NO_PADDING )
-}
+	return sodium.to_base64(bytes, sodium.base64_variants.URLSAFE_NO_PADDING);
+};
 
-const handleError = (req: Request, res: Response, code: String, message: String) => {
+const handleError = (
+	req: Request,
+	res: Response,
+	code: String,
+	message: String
+) => {
 	const path = req.url.toUpperCase();
-	logMessage(req, `${ code }: ${ message }`);
+	logMessage(req, `${code}: ${message}`);
 	if (res) {
 		res.status(400).json({ code, message });
 	}
-}
+};
 
 const logMessage = (req: Request, message: String) => {
-	console.log(`${ req.url.toUpperCase() }: ${ message }` );
-}
+	console.log(`${req.url.toUpperCase()}: ${message}`);
+};
 
 // function distillError(error: IStripeError) {
 // 	return (({ code, message, param, type }) => ({ code, message, param, type }))(error)
@@ -282,14 +373,14 @@ function close() {
 	expressServer.close();
 }
 
-function getUsers () {
+function getUsers() {
 	return users;
 }
 
 let mocha_callback: () => {};
 
 function setMochaCallback(callback: () => {}) {
-	 mocha_callback = callback;
+	mocha_callback = callback;
 }
 
-module.exports = { app, setMochaCallback, close, getUsers }
+module.exports = { app, setMochaCallback, close, getUsers, users, handleStripeWebhook, handleError }
--- test/test-confirm-email.js
+++ test/test-confirm-email.js
@@ -17,101 +17,103 @@ const server = app.app;
 chai.use(chaiHttp);
 chai.use(sinonChai);
 
-let users;
+let users, ngrokUrl;
 before(async () => {
 	await sodium.ready;
 	users = app.getUsers();
+	//  users = app.users;
+
 }); // This depends on mochaInit.js to work
 
 const handleError = res =>
 	console.log(`MOCHA/CHAI: ${res.body.code}: ${res.body.message}`);
 
 describe("--- TESTING EMAIL CONFIRMATION ---", () => {
-	describe("-- Confirming an account with email that doesn't exist --", () => {
-		it("Should return 400 with code: EmailConfirmationError", async () => {
-			// Setup
-			await users.insertOne({
-				email: "justinfrancos@gmail.com",
-				confirmation_key: "confirmation_key",
-				signing_key: Buffer.from(sodium.crypto_auth_keygen())
+		describe("-- Confirming an account with email that doesn't exist --", () => {
+			it("Should return 400 with code: EmailConfirmationError", async () => {
+				// Setup
+				await users.insertOne({
+					email: "justinfrancos@gmail.com",
+					confirmation_key: "confirmation_key",
+					signing_key: Buffer.from(sodium.crypto_auth_keygen())
+				});
+				const spy = sinon.spy(console, "log");
+
+				// Exercise
+				const res = await chai
+					.request(server)
+					.post("/confirm_email")
+					.send({ key: "confirmation_key", email: "asdf@asdf.com" });
+
+				// Verify
+				const user = await users.findOne({ email });
+				user.should.contain.key("confirmation_key");
+				res.should.have.status(400);
+				spy.should.have.been.calledWithMatch("MissingUserError");
+				res.body.should.have.property("code", "EmailConfirmationError");
+
+				// Teardown
+				await users.drop();
+				console.log.restore();
 			});
-			const spy = sinon.spy(console, "log");
-
-			// Exercise
-			const res = await chai
-				.request(server)
-				.post("/confirm_email")
-				.send({ key: "confirmation_key", email: "asdf@asdf.com" });
-
-			// Verify
-			const user = await users.findOne({ email });
-			user.should.contain.key("confirmation_key");
-			res.should.have.status(400);
-			spy.should.have.been.calledWithMatch("MissingUserError");
-			res.body.should.have.property("code", "EmailConfirmationError");
-
-			// Teardown
-			await users.drop();
-			console.log.restore();
 		});
-	});
 
-	describe("-- Confirming an account with a user that already confirmed --", () => {
-		it("Should return 400 with code: EmailConfirmationError", async () => {
-			// Setup
-			await users.insertOne({
-				email: "justinfrancos@gmail.com"
-				//				signing_key: Buffer.from (sodium.crypto_auth_keygen())
+		describe("-- Confirming an account with a user that already confirmed --", () => {
+			it("Should return 400 with code: EmailConfirmationError", async () => {
+				// Setup
+				await users.insertOne({
+					email: "justinfrancos@gmail.com"
+					//				signing_key: Buffer.from (sodium.crypto_auth_keygen())
+				});
+				const spy = sinon.spy(console, "log");
+
+				// Exercise
+				const res = await chai
+					.request(server)
+					.post("/confirm_email")
+					.send({ key: "confirmation_key", email: "asdf@asdf.com" });
+
+				// Verify
+				const user = await users.findOne({ email });
+				user.should.not.contain.key("confirmation_key");
+				res.body.should.have.property("code", "EmailConfirmationError");
+				res.should.have.status(400);
+				spy.should.have.been.calledWithMatch("MissingUserError");
+
+				// Teardown
+				await users.drop();
+				console.log.restore();
 			});
-			const spy = sinon.spy(console, "log");
-
-			// Exercise
-			const res = await chai
-				.request(server)
-				.post("/confirm_email")
-				.send({ key: "confirmation_key", email: "asdf@asdf.com" });
-
-			// Verify
-			const user = await users.findOne({ email });
-			user.should.not.contain.key("confirmation_key");
-			res.body.should.have.property("code", "EmailConfirmationError");
-			res.should.have.status(400);
-			spy.should.have.been.calledWithMatch("MissingUserError");
-
-			// Teardown
-			await users.drop();
-			console.log.restore();
 		});
-	});
 
-	describe("-- Confirming an account with incorrect key --", () => {
-		it("Should return 400 with code: ConfirmationKeyError", async () => {
-			// Setup
-			await users.insertOne({
-				email: "justinfrancos@gmail.com",
-				confirmation_key: "confirmation_key",
-				signing_key: Buffer.from(sodium.crypto_auth_keygen())
+		describe("-- Confirming an account with incorrect key --", () => {
+			it("Should return 400 with code: ConfirmationKeyError", async () => {
+				// Setup
+				await users.insertOne({
+					email: "justinfrancos@gmail.com",
+					confirmation_key: "confirmation_key",
+					signing_key: Buffer.from(sodium.crypto_auth_keygen())
+				});
+				const spy = sinon.spy(console, "log");
+
+				// Exercise
+				const res = await chai
+					.request(server)
+					.post("/confirm_email")
+					.send({ key: "confirmation_notkey", email });
+
+				// Verify
+				const user = await users.findOne({ email });
+				user.should.contain.key("confirmation_key");
+				res.should.have.status(400);
+				spy.should.have.been.calledWithMatch("ConfirmationKeyError");
+				res.body.should.have.property("code", "EmailConfirmationError");
+
+				// Teardown
+				await users.drop();
+				console.log.restore();
 			});
-			const spy = sinon.spy(console, "log");
-
-			// Exercise
-			const res = await chai
-				.request(server)
-				.post("/confirm_email")
-				.send({ key: "confirmation_notkey", email });
-
-			// Verify
-			const user = await users.findOne({ email });
-			user.should.contain.key("confirmation_key");
-			res.should.have.status(400);
-			spy.should.have.been.calledWithMatch("ConfirmationKeyError");
-			res.body.should.have.property("code", "EmailConfirmationError");
-
-			// Teardown
-			await users.drop();
-			console.log.restore();
 		});
-	});
 
 	describe("-- Confirming a newly created account with valid parameters --", () => {
 		it("Should return 200", async () => {
@@ -120,6 +122,60 @@ describe("--- TESTING EMAIL CONFIRMATION ---", () => {
 				.request(server)
 				.post("/signup")
 				.send({ email, password, source: token });
+			let user = await users.findOne({ email });
+			const key = user.confirmation_key;
+			//const spy = sinon.spy(app.handleStripeWebhook);
+
+			// Exercise
+			const res = await chai
+				.request(server)
+				.post("/confirm_email")
+				.send({ key, email });
+
+			// Wait for Stripe Webhooks to arrive
+			console.log('Awaiting subscription via webhook')
+			await new Promise(resolve => {
+				const stream = users.watch()
+				stream.on('change', async data => {
+					user = await users.findOne({ email });
+		 			if (user.monthly_prints === 5) {
+						resolve();
+						stream.close(); // does this unwatch the stream??
+					}
+				})	
+			})
+
+			// Verify
+			res.should.have.status(200);
+			user.should.not.contain.key("confirmation_key");
+			user.should.have.property("monthly_prints", 5);
+
+			// Teardown
+			await users.drop();
+		}).timeout(20000);
+	});
+
+	// describe("timing test", () => {
+	// 	it ('asdf', async () => {
+	// 		await new Promise(resolve =>
+	// 			setTimeout(resolve, 3000)
+	// 		);
+	// 		after(async () => {
+	// 			await new Promise(resolve =>
+	// 				setTimeout(resolve, 2000)
+	// 			);
+	// 		});
+	// 	}).timeout(4000);
+	// })
+
+	describe("-- Confirming a newly created account with tok_chargeCustomerFail --", () => {
+		it("Should return 200", async () => {
+			// Setup
+			// email = "asdf@asdf.com";
+			await chai
+				.request(server)
+				.post("/signup")
+				.send({ email, password, source: "tok_chargeCustomerFail" });
 			let user = await users.findOne({ email });
 			const key = user.confirmation_key;
 
@@ -140,12 +196,13 @@ describe("--- TESTING EMAIL CONFIRMATION ---", () => {
 	});
 
 	describe("-- Confirming a newly created account with bad stripe_id --", () => {
-		it("Should return 200", async () => {
+		it("Should return 200", async () => { // why would i even make this a test?
 			// Setup
 			await users.insertOne({
-				email: "justinfrancos@gmail.com",
+				email,
 				confirmation_key: "confirmation_key",
 				stripe_id: "asdf",
+				current_period_end: 0,
 				signing_key: Buffer.from(sodium.crypto_auth_keygen())
 			});
 
